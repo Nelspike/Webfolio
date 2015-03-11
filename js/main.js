6e3f86c3e1bdafeea7e4700ef5a681e3c8997fe4
Nelspike <nelspiketx@gmail.com>
2015-03-11 23:06:50 +0100

    Added Hand Movement.

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/**
+ * HashMap that contains all of the key stroke information.
+ *
+ */
+var keyMap = {37: "left", 38: "up", 39: "right", 40: "down",
+  65: "left", 68: "right", 83: "down", 87: "up"};
+
+/**
+ * Moves the selector from one position to another.
+ */
+function handPosition(current, key) {
+  var next = 0;
+  switch(key) {
+    case "left":
+      next = current % 2 == 0 ? current + 1 : current - 1;
+      break;
+    case "right":
+      next = current % 2 == 0 ? current + 1 : current - 1;
+      break;
+    case "up":
+      next = current == 0 || current == 1 ? current + 4 : current - 2;
+      break;
+    case "down":
+      next = current == 4 || current == 5 ? current - 4 : current + 2;
+      break;
+  }
+
+  $(".selector").data("position", next);
+  $(".selector").detach().appendTo(".menu-image[data-position="+next+"]");
+}
+
+/**
+ * Controls the handler for the hand movement on the Main Menu.
+ *
+ */
+function handMovement() {
+  $(document).keydown(function(event) {
+    var current = $(".selector").data("position");
+    var key = keyMap[event.which];
+    handPosition(current, key);
+  });
+}
+
+$(document).ready(function() {
+  handMovement();
+});
