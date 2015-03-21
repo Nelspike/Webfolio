cbb638b94b5f111dc5effa041413ebe8c2f49e97
Nelspike <nelspiketx@gmail.com>
2015-03-21 17:38:22 +0100

    Added static menus to the website.

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -5,14 +5,21 @@
  * HashMap that contains all of the key stroke information.
  *
  */
-var keyMap = {37: "left", 38: "up", 39: "right", 40: "down",
+var keyMap = {13: "menu", 37: "left", 38: "up", 39: "right", 40: "down",
   65: "left", 68: "right", 83: "down", 87: "up"};
+
+/**
+ * Static Menu for Inner Pages.
+ */
+var bottom = '<div class="bottom-menu"></div>';
 
 /**
  * Moves the selector from one position to another.
  */
 function handPosition(current, key) {
   var next = 0;
+  var arrow = true;
+
   switch(key) {
     case "left":
       next = current % 2 == 0 ? current + 1 : current - 1;
@@ -26,10 +33,20 @@ function handPosition(current, key) {
     case "down":
       next = current == 4 || current == 5 ? current - 4 : current + 2;
       break;
+    case "menu":
+      arrow = !arrow;
+      break;
   }
 
-  $(".selector").data("position", next);
-  $(".selector").detach().prependTo(".menu-image[data-position="+next+"]");
+  if (arrow) {
+    $(".selector").data("position", next);
+    $(".selector").detach().prependTo(".menu-image[data-position="+next+"]");
+  }
+  else {
+    $.get("/statics/" + current + ".html", function(data) {
+      $("body").html(data + bottom);
+    });
+  }
 }
 
 /**
